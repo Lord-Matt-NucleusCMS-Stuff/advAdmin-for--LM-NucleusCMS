61a6837c86159d75f94b0a38acde318612bf3d96
lordmatt <lordmatt@Hope>
2014-06-14 16:08:36 +0100

    Added two new methods for merging simpleXML elements. One uses DOM and the other iterates. In theory they are both as fast as each other. YMMV.

--- advAdmin/advAdmin_simpleXML.php
+++ advAdmin/advAdmin_simpleXML.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * advAdmin_simpleXML gives you extra editing power
+ * 
+ * This edition released with advAdmin 1.1
  *
  * @author lordmatt
  */
@@ -11,11 +13,11 @@ class advAdmin_simpleXML extends SimpleXMLElement {
      * 
      * See also: http://stackoverflow.com/questions/2092012/simplexml-how-to-prepend-a-child-in-a-node
      * 
-     * @param type $name
-     * @param type $value
-     * @return type 
+     * @param string $name
+     * @param string $value
+     * @return advAdmin_simpleXML (SimpleXMLElement)
      */
-    public function prependChild($name, $value)
+    public function prependChild($name, $value='')
     {
         $dom = dom_import_simplexml($this);
 
@@ -26,4 +28,51 @@ class advAdmin_simpleXML extends SimpleXMLElement {
 
         return simplexml_import_dom($new, get_class($this));
     }
+    
+    /**
+     * Imports an XML element from elsewhere and adds it to the tree using DOM.
+     * 
+     * See also: http://stackoverflow.com/questions/3418019/simplexml-append-one-tree-to-another
+     * 
+     * @param simpleXML $child
+     * @return simpleXML (advAdmin) 
+     */
+    public function importChild($child){
+        // in case some idiot gives us a string
+        if(is_string($child)){
+            $this->addChild($child);
+        }
+        
+        $domBit = dom_import_simplexml($child);
+        $Dself   = dom_import_simplexml($this);    
+        
+        $domBit = $Dself->ownerDocument->importNode($domBit, TRUE);
+        $Dself->appendChild($domBit);
+        
+        return simplexml_import_dom($Dself, get_class($this));
+
+    }
+    
+
+    /** 
+     * Copy the given SimpleXMLElement into this one itteratively 
+     * FROM: http://us2.php.net//manual/en/simplexmlelement.addchild.php
+     * @param SimpleXMLElement $append 
+     */ 
+    public function appendXML($append) 
+    { 
+        if ($append) { 
+            if (strlen(trim((string) $append))==0) { 
+                $xml = $this->addChild($append->getName()); 
+                foreach($append->children() as $child) { 
+                    $xml->appendXML($child); 
+                } 
+            } else { 
+                $xml = $this->addChild($append->getName(), (string) $append); 
+            } 
+            foreach($append->attributes() as $n => $v) { 
+                $xml->addAttribute($n, $v); 
+            } 
+        } 
+    } 
 }
